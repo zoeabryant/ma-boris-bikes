7b71f6fcc90f2db0324ebae09ce20b8d604c03fc
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-07-09 15:15:48 +0100

    bike container

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -6,9 +6,40 @@ describe BikeContainer do
 	let(:billy) { Bike.new }
 	let(:holder) { ContainerHolder.new }
 
+	def fill_station(station)
+		10.times { station.dock(billy) }
+	end
+
 	it 'should accept a bike' do
 		expect(holder.bike_count).to eq(0)
 		holder.dock(billy)
 		expect(holder.bike_count).to eq(1)
 	end
+
+	it 'should release a bike' do
+		holder.dock(billy)
+		holder.release(billy)
+		expect(holder.bike_count).to eq(0)
+	end
+
+	it 'should know when it is full' do
+		expect(holder).not_to be_full
+		fill_station(holder)
+		expect(holder).to be_full
+	end
+
+	it 'should not accept a bike if at capacity' do
+		fill_station(holder)
+		expect(lambda {holder.dock(billy)}).to raise_error(RuntimeError)
+	end
+
+	it 'should provide the list of avaliable bikes' do
+		working_bike, broken_bike = Bike.new, Bike.new
+		broken_bike.break!
+		holder.dock(working_bike)
+		holder.dock(broken_bike)
+
+		expect(holder.available_bikes).to eq([working_bike])
+	end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,42 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
-	let (:billy) { Bike.new }
-	let (:moorgate) { DockingStation.new }
+	let (:moorgate) { DockingStation.new(:capacity => 123) }
 
-	def fill_station(station)
-		10.times { station.dock(billy) }
-	end
-
-	it 'should accept a bike' do
-		expect(moorgate.bike_count).to eq(0)
-		moorgate.dock(billy)
-		expect(moorgate.bike_count).to eq(1)
-	end
-
-	it 'should release a bike' do
-		moorgate.dock(billy)
-		moorgate.release(billy)
-		expect(moorgate.bike_count).to eq(0)
-	end
-
-	it 'should know when it is full' do
-		expect(moorgate).not_to be_full
-		fill_station(moorgate)
-		expect(moorgate).to be_full
-	end
-
-	it 'should not accept a bike if at capacity' do
-		fill_station(moorgate)
-		expect(lambda {moorgate.dock(billy)}).to raise_error(RuntimeError)
-	end
-
-	it 'should provide the list of avaliable bikes' do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break!
-		moorgate.dock(working_bike)
-		moorgate.dock(broken_bike)
-
-		expect(moorgate.available_bikes).to eq([working_bike])
+	it 'should allow setting default capacity on initialising' do
+		expect(moorgate.capacity).to eq(123)
 	end
 end

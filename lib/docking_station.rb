600abbb84dd8944512669476e729c0f9f393a952
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-07-09 12:32:15 +0100

    Docking station init and progress

---
+++ lib/docking_station.rb
@@ -0,0 +1,19 @@
+class DockingStation
+
+	def initialize
+		@bikes = []
+	end
+
+	def bike_count
+		@bikes.count
+	end
+
+	def dock(bike)
+		@bikes << bike
+	end
+
+	def release(bike)
+		@bikes.delete(bike)
+	end
+
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,21 @@
+require 'docking_station'
+
+describe DockingStation do
+
+	it 'should accept a bike' do
+		bike = Bike.new
+		station = DockingStation.new
+
+		expect(station.bike_count).to eq(0)
+		station.dock(bike)
+		expect(station.bike_count).to eq(1)
+	end
+
+	it 'should release a bike' do
+		bike = Bike.new
+		station = DockingStation.new
+		station.dock(bike)
+		station.release(bike)
+		expect(station.bike_count).to eq(0)
+	end
+end
